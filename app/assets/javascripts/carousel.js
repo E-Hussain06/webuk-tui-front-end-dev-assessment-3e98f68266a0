48be2b3f4ab5df5b824a07e7a1e3e7f7e49370ac
Ehsan Hussain <ehsanhussain991@gmail.com>
2016-09-10 12:32:56 +0100

    Updated carousel.js
    Removed singleSlideHeight
    Commented code further.

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -5,15 +5,15 @@ var carousel = {
     singleSlideHeight: null,
     currentSlide: null,
 
+    // This method is responsible for initializing the carousel
     setup: function() {
         this.container          = $("#content");                            // Carousel.container = content
-        this.totalSlides        = $(this.container).children().size();      // Size of the Object Array, in this case the Carousel size relative to the array. 
         this.singleSlideHeight  = $(this.container).children().height();    // Size of the height of the select elements, in this case the child elements of Carousel
-        this.menu();
-        this.goToSlide(1);
+        this.menu();                                                        // Setup of the side-menu
+        this.goToSlide(1);                                                  // Setup Carousel and shows first slide
     },
     
-    // The goToSlide function is responsible for animating to the next chosen slide, which also takes a parameter/argument.
+    // The goToSlide method is responsible for animating to the next chosen slide, which also takes a parameter/argument.
     goToSlide: function(slideNum) {
         
         $(carousel.container).children().each(function(index) { 
@@ -42,7 +42,7 @@ var carousel = {
             $(this).click(function() {
                 carousel.goToSlide(index + 1);
 
-                // Adding and removing class for the menu hightlight-er. CSS takes control of its positioning
+                // Adding and removing class for the menu hightlight-er. CSS takes control of it's positioning/transitioning
                 $('#menu').removeAttr("class").addClass("highlight-step-" + index );
             });
 
